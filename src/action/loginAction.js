5b153477acb25f32968f82da448a1fd5ee923b8b
Rahul Biswal <biswalrahul787@gmail.com>
2023-10-17 08:11:08 +0530

    created login action

--- src/action/loginAction.js
+++ src/action/loginAction.js
@@ -1,6 +1,12 @@
-export const loginaction = (data) => (dispatch) => {
-   dispatch({
+// export const loginaction = (data) => (dispatch) => {
+//    dispatch({
+//     type:"BOLT_ON",
+//     payload:data
+//    })
+// }
+export const loginaction = (data) => {
+ return  {
     type:"BOLT_ON",
     payload:data
-   })
+   }
 }
